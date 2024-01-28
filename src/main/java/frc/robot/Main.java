843dade8a44700fb3f1529b81187d4cacdfcc78c
Tim Helland <tim.helland@lebanon.k12.or.us>
2024-01-28 10:50:09 -0800

    Even more empty :)

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -7,9 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
-
-  public static void main(String... args) {
     //TODO: Start the robot program
   
-  }
 }
